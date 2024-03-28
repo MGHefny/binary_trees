1b7984621cf219b1193c46373e24cd5a12e0fcdd
MGHefny <mgenedhefny@gmail.com>
2024-03-28 21:05:52 +0200

    11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - size binary tree
+ * @tree: pointer
+ * Return: size tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t s = 0;
+
+	if (tree)
+	{
+		s += 1;
+		s += binary_tree_size(tree->left);
+		s += binary_tree_size(tree->right);
+	}
+	return (s);
+}
